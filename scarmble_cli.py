3f9b53d3c2f91205d08ba37696c38e6f1eb72aff
lSANCHOl <60328198+lSANCHOl@users.noreply.github.com>
2021-04-07 00:57:21 +0100

    Create scarmble_cli.py
    version where flags are used

---
+++ scarmble_cli.py
@@ -0,0 +1,249 @@
+#!/bin/python3
+
+import argparse
+import sys
+import random
+ckey = "abcdefghijklmnopqrstuvwxyz1234567890!£$%^&*()-_=+[{]};:'@#~|,<.>/? ABCDEFGHIJKLMNOPQRSTUVWXYZ\""
+
+
+def keygen(key):
+    
+    cba = ""
+    #print(len(abc))
+    length = len(key)
+    for i in range(94):		
+        rannum = random.randint(0,length-1)
+        rancha = key[rannum]
+        cba += rancha
+        key = key.replace(rancha, "")
+        length = len(key)
+    return cba
+  
+
+def encode(key,message,output):
+    out = ""
+     
+    for i in range(len(message)):
+        for j in range(len(ckey)):
+            if message[i] == ckey[j]:
+                tmp = key[j]
+                out += tmp
+   
+    if output == True:
+        print("[+]",out,"[+]")
+    elif output == False:
+        file1 = open("encoded.txt", 'w')     
+        n = file1.write(out)
+        file1.close()
+    else:
+        print("[+]",out,"[+]")
+        file1 = open("encoded.txt", 'w')
+        n = file1.write(out)
+        file1.close()
+				
+def decode(key,message,output):
+    dkey = "" 
+    for i in range(len(key)-1): 
+        loc1 = ckey[i]
+        loc2 = key.find(loc1)
+        loc3 = ckey[loc2]
+        dkey += loc3
+    out = ""
+
+    for i in range(len(message)):
+        for j in range(len(ckey)):
+            if message[i] == ckey[j]:
+                tmp = dkey[j]
+                out += tmp 
+
+    if output == True:
+        print("[+]",out,"[+]")
+    elif output == False:
+        file1 = open("decoded.txt", 'w')     
+        n = file1.write(out)
+        file1.close()
+    else:
+        print("[+]",out,"[+]")
+        file1 = open("decoded.txt", 'w')
+        n = file1.write(out)
+        file1.close()
+	
+
+
+
+
+parser = argparse.ArgumentParser(description='Encode and Decode messages with a unique key')    
+parser.add_argument('-k', '--Key', metavar='', help='Path to unique key')
+parser.add_argument('-e', '--Encode', action='store_true', help='encode')
+parser.add_argument('-d', '--Decode', action='store_true', help='decode')
+parser.add_argument('-f', '--File', metavar='', help='path to file to encode/decode')
+parser.add_argument('-p', '--Print', action='store_true', help='print encoded/decoded message')
+parser.add_argument('-s', '--Store', action='store_true', help='store encoded/decoded message to file')
+parser.add_argument('-b', '--Both', action='store_true', help='print and store encoded/decoded message')
+parser.add_argument('-K', '--Keygen', action='store_true',  help='generate unique key')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+   
+    if args.Encode and args.Print and args.Key:
+        output = True
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key 
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+
+
+    
+    elif args.Encode and args.Store and args.Key:
+        output = False
+        filename =args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+
+     
+    elif args.Encode and args.Store and args.Key:
+        output = 2
+        filename = args.File 
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key 
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+    
+    
+ 
+    elif args.Encode and args.Print and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = True
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+
+
+    
+    elif args.Encode and args.Store and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = False
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+
+     
+    elif args.Encode and args.Both and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = 2
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        encode(keycontents,filecontents,output)
+    
+    
+    if args.Decode and args.Print and args.Key:
+        output = True
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        decode(keycontents,filecontents,output)
+
+
+    
+    elif args.Decode and args.Store and args.Key:
+        output = False
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        decode(keycontents,filecontents,output)
+
+     
+    elif args.Decode and args.Store and args.Key:
+        output = 2
+        filename = args.File 
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        keyname = args.Key 
+        with open(keyname, 'r') as file:
+            keycontents = file.read()
+            file.close()
+        decode(keycontents,filecontents,output)
+    
+    
+ 
+    elif args.Decode and args.Print and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = True
+        filename = args.File 
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        decode(keycontents,filecontents,output)
+
+
+    
+    elif args.Decode and args.Store and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = False
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        decode(keycontents,filecontents,output)
+
+     
+    elif args.Decode and args.Both and args.Keygen:
+        keycontents = keygen(ckey)
+        print("[KEY]",keycontents,"[KEY]")
+        output = 2
+        filename = args.File
+        with open(filename, 'r') as file:
+            filecontents = file.read()
+            file.close()
+        Decode(keycontents,filecontents,output)
+    
+    elif args.Keygen and args.Print:
+        print(keygen(ckey))
+    elif args.Keygen and args.Store:
+        key = keygen(ckey)
+        file1 = open("KEY", 'w')
+        file1.write(key)
+        file1.close()
+    elif args.Keygen and args.Both:
+        key = keygen(ckey)
+        print(key)
+        file1 = open("KEY", 'w')
+        file1.write(key)
+        file1.close()
